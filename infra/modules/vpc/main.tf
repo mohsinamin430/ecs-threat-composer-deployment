509458fa58d4f208e4e113ec5b350f9e5e797dc3
mohsinamin430 <mohsinamin430@gmail.com>
2026-07-09 22:37:28 +0100

    readme updated and testing regional nat gateway

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -43,7 +43,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat_gateway_threatapp" {
     allocation_id = aws_eip.nat_eip.id
-    subnet_id = aws_subnet.public_subnet_1.id
+    vpc_id = aws_vpc.vpc_threatapp.id
 }
 
 
